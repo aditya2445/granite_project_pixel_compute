2360bf105af6cd6e2edb089bb39a0ae82a082369
Aditya Mohaty <adityamohaty@Adityas-MacBook-Pro.local>
2025-06-28 02:54:43 +0530

    Set up react environment

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,10 @@
-// Entry point for the build script in your package.json
-import "../stylesheets/application.scss"
-import "@hotwired/turbo-rails"
-import "./controllers"
+// // Entry point for the build script in your package.json
+// import "../stylesheets/application.scss"
+// import "@hotwired/turbo-rails"
+// import "./controllers"
+import { initializeLogger } from "../src/common/logger";
+import "../stylesheets/application.scss";
+
+import { setAuthHeaders } from "apis/axios";
+initializeLogger();
+setAuthHeaders();
