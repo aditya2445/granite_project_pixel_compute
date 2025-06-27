39a5db8b9deb534bc6b66176ed8c98f99011666e
Aditya Mohaty <adityamohaty@Adityas-MacBook-Pro.local>
2025-06-28 02:41:02 +0530

    Setup ESBuild and Vite

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,11 @@
 class TasksController < ApplicationController
-  respond_to :html, :xml, :json
-
   def index
     @tasks = Task.all
-    respond_with(@tasks)
+
+    respond_to do |format|
+      format.html 
+      format.json { render json: @tasks }
+      format.xml  { render xml: @tasks }
+    end
   end
 end
