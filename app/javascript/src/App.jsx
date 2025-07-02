610fd9d13a1b4296504760e97288dde25a3b338f
Aditya Mohaty <adityamohaty@Adityas-MacBook-Pro.local>
2025-07-02 23:54:20 +0530

    Added ability to update a task

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { CreateTask, ShowTask } from "components/Tasks";
+import { CreateTask, ShowTask, EditTask } from "components/Tasks";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
@@ -14,6 +14,7 @@ const App = () => (
       <Route exact component={Dashboard} path="/dashboard" />
       <Route exact component={CreateTask} path="/tasks/create" />
       <Route exact component={ShowTask} path="/tasks/:slug/show" />
+      <Route exact component={EditTask} path="/tasks/:slug/edit" />
     </Switch>
   </Router>
 );
--- app/javascript/src/apis/tasks.js
+++ app/javascript/src/apis/tasks.js
@@ -3,7 +3,10 @@ import axios from "axios";
 const fetch = () => axios.get("/tasks");
 const create = payload => axios.post("/tasks", { task: payload });
 const show = slug => axios.get(`/tasks/${slug}`);
-
-const tasksApi = { fetch, create, show };
+const update = ({ slug, payload }) =>
+  axios.put(`/tasks/${slug}`, {
+    task: payload,
+  });
+const tasksApi = { fetch, create, show, update };
 
 export default tasksApi;
