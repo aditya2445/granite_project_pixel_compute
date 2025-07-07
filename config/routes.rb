8a2322652908a0c12652681a6a8ef67f60942135
Aditya Mohaty <adityamohaty@Adityas-MacBook-Pro.local>
2025-07-07 23:41:45 +0530

    Added comments to tasks

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
     resources :tasks, except: %i[new edit], param: :slug
     resources :users, only: %i[index create]
     resource :session, only: [:create, :destroy]
+    resources :comments, only: :create
   end
   root "home#index"
   get "*path", to: "home#index", via: :all
